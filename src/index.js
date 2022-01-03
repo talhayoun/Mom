8d84886acfb6b679b03f845895aab1c472244f29
Tal <talhayoun8@gmail.com>
2022-01-03 16:17:22 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT;
@@ -9,12 +10,16 @@ require("./db/mongoose");
 const userRouter = require("../src/routers/userRouter");
 const mealRouter = require("../src/routers/mealsRouter");
 
-app.get("/", (req, res) => { res.send("ok") })
 app.use(cors());
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, "..", "frontend")))
 app.use(userRouter);
 app.use(mealRouter);
+
+app.get("*", (req, res) => {
+    console.log(path.join(__dirname, "..", "frontend", "index.html"))
+    res.sendFile(path.join(__dirname, "..", "frontend", "index.html"))
+})
 
 app.listen(port, () => {
     console.log('Server connected: ', port)
