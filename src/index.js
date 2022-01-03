78c9fb68af5fcb8edeaefb893bab4d7da543a96b
Tal <talhayoun8@gmail.com>
2022-01-03 15:39:11 +0200

    project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT;
+
+require("./db/mongoose");
+
+const userRouter = require("../src/routers/userRouter");
+const mealRouter = require("../src/routers/mealsRouter");
+
+app.use(cors());
+app.use(express.json())
+
+app.use(userRouter);
+app.use(mealRouter);
+
+app.listen(port, () => {
+    console.log('Server connected: ', port)
+})
